2ba2105f9d3475dfb27042cccd43cd73b17c8715
mizech <michael.zech@hotmail.de>
2025-01-26 16:02:02 +0100

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Michael on 26.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
